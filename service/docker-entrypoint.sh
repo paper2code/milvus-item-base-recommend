9416fecd2e8de2cb11c428fbb79566220eda334c
x0rzkov <x0rzkov@protonmail.com>
2020-09-02 09:26:41 +0000

    Fixtures

--- service/docker-entrypoint.sh
+++ service/docker-entrypoint.sh
@@ -4,4 +4,4 @@ cd /opt/service/model
 nohup bert-serving-start -model_dir uncased_L-12_H-768_A-12 -num_worker=12 > bss.out &
 
 cd /opt/service
-uvicorn main:app
+uvicorn main:app --host 0.0.0.0 --port 8000
